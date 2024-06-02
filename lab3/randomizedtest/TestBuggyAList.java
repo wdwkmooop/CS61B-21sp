0ad429afa3d63aa65c9a42c592d7a9e554eee4ed
wangdawei <994201763@qq.com>
2024-06-02 20:45:31 +0800

    lad3 is donw

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,52 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove(){
+        AListNoResizing<Integer> goodList = new AListNoResizing<>();
+        BuggyAList<Integer> badList = new BuggyAList<>();
+        for(int i=0;i<3;i++){
+                goodList.addLast(i);
+                badList.addLast(i);
+        }
+        for(int i=0;i<3;i++){
+            int a =goodList.removeLast();
+            int b = badList.removeLast();
+            assertEquals(a, b);
+        }
+
+    }
+
+    @Test
+    public void randomizedTest(){
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> buggyAList = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                buggyAList.addLast(randVal);
+                System.out.println("addLast(" + randVal + ")");
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                assertEquals(size, buggyAList.size());
+                System.out.println("size: " + size);
+            } else if (L.size()!=0 && operationNumber == 2){
+                int lastVal = L.removeLast();
+                assertEquals("last val not correct!", lastVal, (int)buggyAList.removeLast());
+                assertEquals("size not correct!", L.size(), buggyAList.size());
+                System.out.printf("removed(%d), size %d -> %d \n", lastVal, L.size()+1, L.size());
+            } else if (L.size()!=0 && operationNumber == 3 ){
+                int lastVal = L.getLast();
+                assertEquals("last val not correct!", lastVal, (int)buggyAList.getLast());
+                assertEquals("unexpectedly modified the size!", buggyAList.size(), L.size());
+                System.out.printf("get last is %d \n", lastVal);
+            }
+        }
+    }
 }
